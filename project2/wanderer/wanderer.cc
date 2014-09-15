ee85f0dcbf5d3bb6353be02958c3be72e18fd812
davefeilseifer <dave@cse.unr.edu>
2014-09-15 01:19:15 -0700

    project2: changes to make project compile/run properly; fixed pseudocode to be more clear

--- project2/wanderer/wanderer.cc
+++ project2/wanderer/wanderer.cc
@@ -2,54 +2,92 @@
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 
-double front_dist;
-double left_dist;
-double right_dist;
+// these global variables are what you will set to complete the project
+double front_dist;  // if set correctly, this will be the closest obstacle to the front of the robot
+double left_dist; // if set correctly, this will be the closest obstacle to the left of the robot
+double right_dist; // if set correctly, this will be the closest obstacle to the right of the robot
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-  front_dist = 99999999999999999;
+	// set front, left, and right distances to be really large
+
+	front_dist = 99999999999999999;
+	left_dist  = 99999999999999999;
+	right_dist = 99999999999999999;
+
+	// loop through the ranges in the sensor scan, one at a time
+	// answer for each range point
+
+	for( int i = 0; i < msg->ranges.size(); i++ )
+	{
+		float angle = msg->angle_min + i*msg->angle_increment;
+		double xdist = msg->ranges[i] * sin(angle);
+		double ydist = msg->ranges[i] * cos(angle);
+
+			// place your code here
+		/*********************************/
+
+			// is the x/y coordinate for this range point
+			// closer than the closest front point so far?
+
+				// if so, set the front_distance to be
+				// the current distance
 
 
-  for( int i = 0; i < msg->ranges.size(); i++ )
-  {
-    float angle = msg->angle_min + i*msg->angle_increment;
-    double xdist = msg->ranges[i] * sin(angle);
-    double ydist = msg->ranges[i] * cos(angle);
+			// is the x/y coordinate for this range point
+			// closer than the closest right point so far?
 
-  /*********************************/
-    // place your code here
+				// if so, set the right_distance to be
+				// the current distance
 
-  /*********************************/
-  }
+
+			// is the x/y coordinate for this range point
+			// closer than the closest left point so far?
+
+				// if so, set the left_distance to be
+				// the current distance
+
+		/*********************************/
+	}
 }
 
 int main( int argc, char* argv[] )
 {
-  ros::init(argc,argv,"wanderer");
-  ros::NodeHandle nh;
-  ros::Rate loop_rate(10);
+	// this code initializes a connection to the robot
+	ros::init(argc,argv,"wanderer");
+	ros::NodeHandle nh;
+	ros::Rate loop_rate(10);
 
-  ros::Subscriber laser_sub = nh.subscribe("/base_scan", 1000, laserCallback);
-  ros::Publisher cmd_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1000);
-  geometry_msgs::Twist cmd_vel_msg;
-  double des_vel = 1.0;
+	ros::Subscriber laser_sub = nh.subscribe("/base_scan", 1000, laserCallback);
+	ros::Publisher cmd_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1000);
+	geometry_msgs::Twist cmd_vel_msg;
+	double des_vel = 1.0;
 
+	// loop forever
+	while( ros::ok() )
+	{
+		// by default, the robot will move forward at 1 meter / second
+		double lvel = des_vel;
+		
+		// by default, the robot will not turn at all
+		double rvel = 0;
 
-  while( ros::ok() )
-  {
-    double lvel = des_vel;
+			//place your code here
+		/***************************************/
 
-  /***************************************/
-    //place your code here
+			// if there is an obstacle close to the front
+				// slow down
+				// turn to avoid the obstacle			
 
-  /***************************************/
+		/***************************************/
 
-    cmd_vel_msg.linear.x = lvel;
-    cmd_vel.publish(cmd_vel_msg);
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+		// send the speeds to the robot
+		cmd_vel_msg.linear.x = lvel;
+		cmd_vel_msg.angular.z = rvel;
+		cmd_vel.publish(cmd_vel_msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 
-  return 0;
+	return 0;
 }
