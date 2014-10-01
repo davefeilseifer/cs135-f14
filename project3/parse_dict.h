0cddfa0b1c75c49b43a2d6cc4c803aac31571f48
davefeilseifer <dave@cse.unr.edu>
2014-09-30 21:50:52 -0700

    added clearer comments for parse_passwd and parse_dict

--- project3/parse_dict.h
+++ project3/parse_dict.h
@@ -1,6 +1,21 @@
 #ifndef _PARSE_DICT_H_
 #define _PARSE_DICT_H_
 
+/**
+	get_words:
+
+	A function to parse the "words" dictionary file provided for the assignment
+
+	int num_words: an integer (passed by value) that is set by the function
+		that equals the number of words in the dictionary
+
+	char** words: an array of strings (char*) (passed by value) that when the function
+		is completed will contain each word in the dictionary file
+
+	returns: nothing (void)
+
+**/
+
 void get_words( int &num_words, char** &words );
 
 #endif
