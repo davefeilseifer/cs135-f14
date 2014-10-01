f10dcdb0298a15170044c207f8c9ff460f0f68c1
David Feil-Seifer <dave@cse.unr.edu>
2014-10-01 14:01:57 -0700

    that was embarrassing

--- project3/parse_dict.h
+++ project3/parse_dict.h
@@ -6,10 +6,10 @@
 
 	A function to parse the "words" dictionary file provided for the assignment
 
-	int num_words: an integer (passed by value) that is set by the function
+	int num_words: an integer (passed by reference) that is set by the function
 		that equals the number of words in the dictionary
 
-	char** words: an array of strings (char*) (passed by value) that when the function
+	char** words: an array of strings (char*) (passed by reference) that when the function
 		is completed will contain each word in the dictionary file
 
 	returns: nothing (void)
