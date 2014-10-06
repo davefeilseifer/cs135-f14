78473907500bcf999c18894434c5f577e8dfa4e5
davefeilseifer <dave@cse.unr.edu>
2014-10-05 20:47:00 -0700

    first draft of grader code

--- project2/grader/grader.cpp
+++ project2/grader/grader.cpp
@@ -1,8 +1,47 @@
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+
+float old_x = 0;
+float old_y = 0;
+float old_z = 0;
+
+void posCallback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+  float x = msg->pose.pose.position.x;
+  float y = msg->pose.pose.position.y;
+  float z = msg->pose.pose.orientation.z;
+
+  float diffx = fabs( old_x - x );
+  float diffy = fabs( old_y - y );
+  float diffz = fabs( old_z - z );
+
+  //ROS_INFO( "%0.2f %0.2f %0.2f", diffx, diffy, diffz );
+
+  if( diffx > 0.01 || diffy > 0.01 || diffz > 0.01 )
+  {
+    printf( "+" );
+  }
+  else
+  {
+    printf( "-" );
+  }
+
+  fflush(stdout);
+
+  old_x = x;
+  old_y = y;
+  old_z = z;
+}
 
 int main( int argc, char* argv[] )
 {
   ros::init(argc,argv, "grader" );
+  ros::NodeHandle nh;
+  ros::Rate loop_rate(10);
+  // subscribe to robot position data
+  ros::Subscriber pos_sub = nh.subscribe("/odom", 1000, posCallback);
+  // loop forever (no main control code)
+  ros::spin();
 
   return 0;
 }
