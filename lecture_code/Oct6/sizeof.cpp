87f72ca4ef93693bd99caa3cfbf4a3be173cbf32
David Feil-Seifer <dave@cse.unr.edu>
2014-10-06 14:00:57 -0700

    sizeof for lecture

---
+++ lecture_code/Oct6/sizeof.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf( "sizeof (int): \t%lu\n", sizeof(int) );
+	printf( "sizeof (char): \t%lu\n", sizeof(char) );
+	printf( "sizeof (float):\t%lu\n", sizeof(float) );
+	printf( "sizeof (long): \t%lu\n", sizeof(long) );	
+	printf( "sizeof (double):%lu\n", sizeof(double) );	
+	printf( "sizeof (bool): \t%lu\n", sizeof(bool) );	
+	printf( "sizeof (short):\t%lu\n", sizeof(short) );	
+	printf( "sizeof (int*): \t%lu\n", sizeof(int*) );	
+	
+}
