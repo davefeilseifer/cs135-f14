7128fb4e51eba54882d787080c01a203e3282f2f
davefeilseifer <dave@cse.unr.edu>
2014-10-08 22:45:08 -0700

    modified grader to output file

--- project2/grader/grader.cpp
+++ project2/grader/grader.cpp
@@ -5,6 +5,9 @@ float old_x = 0;
 float old_y = 0;
 float old_z = 0;
 
+int plus_count = 0;
+int minus_count = 0;
+
 void posCallback(const nav_msgs::Odometry::ConstPtr &msg)
 {
   float x = msg->pose.pose.position.x;
@@ -17,13 +20,15 @@ void posCallback(const nav_msgs::Odometry::ConstPtr &msg)
 
   //ROS_INFO( "%0.2f %0.2f %0.2f", diffx, diffy, diffz );
 
-  if( diffx > 0.01 || diffy > 0.01 || diffz > 0.01 )
+  if( diffx > 0.01 || diffy > 0.01 || diffz > 0.001 )
   {
-    printf( "+" );
+    //printf( "+" );
+    plus_count++;
   }
   else
   {
-    printf( "-" );
+    //printf( "-" );
+    minus_count++;
   }
 
   fflush(stdout);
@@ -40,8 +45,28 @@ int main( int argc, char* argv[] )
   ros::Rate loop_rate(10);
   // subscribe to robot position data
   ros::Subscriber pos_sub = nh.subscribe("/odom", 1000, posCallback);
-  // loop forever (no main control code)
-  ros::spin();
+
+  // burn a little time
+  loop_rate.sleep();
+  ros::spinOnce();
+  loop_rate.sleep();
+  ros::spinOnce();
+  loop_rate.sleep();
+  ros::spinOnce();
+
+  // loop for two minutes (no main control code)
+  for( int i = 0; i < 1200; i++ )
+  {
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+  FILE* outfile = fopen(".grade", "w" );
+  if( !outfile )
+  {
+    fprintf( stderr, "error opening file for grading\n" );
+  }
+  fprintf( outfile, "%d %d\n", plus_count, minus_count );
+  fclose(outfile);
 
   return 0;
 }
