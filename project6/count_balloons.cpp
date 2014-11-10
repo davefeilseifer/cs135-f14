8c68e13a56288fe3764ae0447c8cc2ef9168b0da
davefeilseifer <dave@cse.unr.edu>
2014-11-10 10:36:16 -0800

    updated array_to_image function

--- project6/count_balloons.cpp
+++ project6/count_balloons.cpp
@@ -1,28 +1,76 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
+#include "count_balloons.h"
 #include <stdio.h>
 
-int main( int argc, char** argv )
-{
-	if( argc != 2)
-    {
-		printf( " Usage: display_image ImageToLoadAndDisplay\n" );
-		return -1;
-    }
 
-    cv::Mat image;
-    image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+
+
+ImageData image_to_array( const char* filename )
+{
+	ImageData ret;
+
+	cv::Mat image;
+    image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);   // Read the file
+
+	ret.height = image.rows;
+	ret.width = image.cols;
 
     if(! image.data )                              // Check for invalid input
     {
         printf("Could not open or find the image\n" );
-        return -1;
+        exit(-1);
     }
 
-    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
-    cv::imshow( "Display window", image );                   // Show our image inside it.
+	// allocate memory for image
+	ret.data = new unsigned char**[ret.height];
+	for(int i = 0; i < ret.height; i++ )
+	{
+		ret.data[i] = new unsigned char*[ret.width];
+		for( int j = 0; j < ret.width; j++ )
+		{
+			ret.data[i][j] = new unsigned char[3];
+		}
+	}
 
-    cv::waitKey(0);                                          // Wait for a keystroke in the window
-    return 0;
+	// set image data
+	for( int i = 0; i < ret.height; i++ )
+	{
+		for( int j = 0; j < ret.width; j++ )
+		{
+			for( int k = 0; k < 3; k++ )
+			{
+				ret.data[i][j][k] = image.at<cv::Vec3b>(i,j)[k];
+			}
+		}
+	}
+
+	return ret;
 }
+
+cv::Mat array_to_image( ImageData data, char* display_name )
+{
+	// initialize image
+	cv::Mat out_image(data.height,data.width, CV_8UC3, cv::Scalar(0,0,0));
+
+	// copy image data to array
+	for( int i = 0; i < data.height; i++ )
+	{
+		for( int j = 0; j < data.width; j++ )
+		{
+			for( int k = 0; k < 3; k++ )
+			{
+				out_image.at<cv::Vec3b>(i,j)[k] = data.data[i][j][k];
+			}
+		}
+	}
+
+	if( display_name != NULL )
+	{
+
+		// display image 
+    	cv::namedWindow( display_name, cv::WINDOW_AUTOSIZE );// Create a window for display.
+	    cv::imshow( display_name, out_image );                   // Show our image inside it.
+
+	}
+	return out_image;
+}
+
---
+++ project6/count_balloons.h
@@ -0,0 +1,26 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+struct ImageData {
+	int width;
+	int height;
+	unsigned char*** data;
+};
+
+/* image_to_array:
+		function to take an image filename, load that image (using OpenCV), and then
+		turn those image data into a 3D array (row, column, channel).
+
+	arguments:
+		const char* filename: the filename of the image file to be loaded
+	
+	returns:
+		struct ImageData: a struct containing the image data that was loaded,
+						  along with the image properties		
+*/
+
+ImageData image_to_array( const char* filename );
+
+cv::Mat array_to_image( ImageData data, char* display_name );
+
