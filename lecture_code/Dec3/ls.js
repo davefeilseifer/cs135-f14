f078c5011d77afb19abc56f0eed14d8dedb65934
Sushil Louis <sushil@ecc-client-machine.rd.unr.edu>
2014-12-03 15:44:45 -0800

    Local Storage

---
+++ lecture_code/Dec3/ls.js
@@ -0,0 +1,12 @@
+
+document.getElementById("stuff").innerHTML = "This is where it starts";
+newName = localStorage.getItem("PlayerName");
+document.getElementById("stuff").innerHTML = "I retrieved: " + newName;
+
+//var name = prompt("What is your name?");
+//document.getElementById("stuff").innerHTML = "This is where it starts: " + name;
+
+//localStorage.setItem("PlayerName", name);
+
+
+
